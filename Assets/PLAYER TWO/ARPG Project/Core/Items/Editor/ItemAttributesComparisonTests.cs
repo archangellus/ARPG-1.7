ee170475a3871b704cc98e990f6e51ad18ad42e7
ANSTUDIO <archangellus@users.noreply.github.com>
2026-09-08 08:42:43 +0300

    Compare socket slots and equipped gems

---
+++ Assets/PLAYER TWO/ARPG Project/Core/Items/Editor/ItemAttributesComparisonTests.cs
@@ -0,0 +1,99 @@
+#if UNITY_EDITOR && UNITY_INCLUDE_TESTS
+using NUnit.Framework;
+using UnityEngine;
+
+namespace PLAYERTWO.ARPGProject.Tests
+{
+    public class ItemAttributesComparisonTests
+    {
+        static readonly Color Favorable = Color.green;
+        static readonly Color Unfavorable = Color.red;
+
+        [Test]
+        public void InspectComparisonShowsSignedMatchedAndGainedDifferences()
+        {
+            var candidate = new ItemAttributes();
+            candidate[ItemAttributes.AttributeType.Strength] = 15;
+            candidate[ItemAttributes.AttributeType.Vitality] = 8;
+            var reference = new ItemAttributes();
+            reference[ItemAttributes.AttributeType.Strength] = 10;
+
+            var text = candidate.InspectComparison(
+                reference,
+                null,
+                null,
+                Favorable,
+                Unfavorable
+            );
+
+            StringAssert.Contains("(+5", text);
+            StringAssert.Contains("(+8", text);
+        }
+
+        [Test]
+        public void InspectComparisonKeepsMissingReferencePropertiesInLossSection()
+        {
+            var candidate = new ItemAttributes();
+            var reference = new ItemAttributes();
+            reference[ItemAttributes.AttributeType.Health] = 40;
+
+            var text = candidate.InspectComparison(
+                reference,
+                null,
+                null,
+                Favorable,
+                Unfavorable
+            );
+
+            StringAssert.Contains("Properties lost when equipped:", text);
+            StringAssert.Contains("- +40 of Additional Health", text);
+        }
+
+        [Test]
+        public void InspectComparisonSuppressesUnchangedAndSocketOnlyDifferences()
+        {
+            var candidate = new ItemAttributes();
+            candidate[ItemAttributes.AttributeType.Dexterity] = 12;
+            candidate[ItemAttributes.AttributeType.Energy] = 5;
+            var reference = new ItemAttributes();
+            reference[ItemAttributes.AttributeType.Dexterity] = 12;
+            var sockets = new ItemAttributes();
+            sockets[ItemAttributes.AttributeType.Energy] = 5;
+
+            var text = candidate.InspectComparison(
+                reference,
+                sockets,
+                null,
+                Favorable,
+                Unfavorable
+            );
+
+            StringAssert.DoesNotContain("(+0", text);
+            StringAssert.DoesNotContain("(-0", text);
+            StringAssert.DoesNotContain("Energy", text);
+        }
+
+        [Test]
+        public void InspectComparisonCanCompareResolvedGemBonusesSeparately()
+        {
+            var candidateGems = new ItemAttributes();
+            candidateGems[ItemAttributes.AttributeType.FireDamage] = 18;
+            var equippedGems = new ItemAttributes();
+            equippedGems[ItemAttributes.AttributeType.FireDamage] = 10;
+            equippedGems[ItemAttributes.AttributeType.Health] = 25;
+
+            var text = candidateGems.InspectComparison(
+                equippedGems,
+                null,
+                null,
+                Favorable,
+                Unfavorable
+            );
+
+            StringAssert.Contains("(+8", text);
+            StringAssert.Contains("Properties lost when equipped:", text);
+            StringAssert.Contains("Additional Health", text);
+        }
+    }
+}
+#endif
